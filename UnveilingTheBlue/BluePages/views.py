08510695621eb65a767167d4a39c0b17fb4d9e01
lalithu <lalithuriti@gmail.com>
2020-11-06 01:20:29 +0530

    Launch and Arrival Times Input Validation

--- UnveilingTheBlue/BluePages/views.py
+++ UnveilingTheBlue/BluePages/views.py
@@ -69,81 +69,91 @@ def simulator(request):
         datetime_arrival = arrival_date
         # print(datetime_arrival)
 
-        # Curiosity: 2011-11-26 15:02, Perserverance: 2020-6-30 11:50
-        date_launch = time.Time(datetime_launch, scale="utc").tdb
-        # Curiosity: 2012-08-06 05:17 Perserverance: 2021-2-18 12:30
-        date_arrival = time.Time(datetime_arrival, scale="utc").tdb
+        def validate(date_text):
+            try:
+                datetime.datetime.strptime(date_text, '%Y-%m-%d')
+                return True
+            except ValueError:
+                return False
 
-        # Earth Launch
-        Earth2dL = OrbitPlotter2D()
+        if validate(datetime_launch) and validate(datetime_arrival):
+            # Curiosity: 2011-11-26 15:02, Perserverance: 2020-6-30 11:50
+            date_launch = time.Time(datetime_launch, scale="utc").tdb
+            # Curiosity: 2012-08-06 05:17 Perserverance: 2021-2-18 12:30
+            date_arrival = time.Time(datetime_arrival, scale="utc").tdb
+            # Earth Launch
+            Earth2dL = OrbitPlotter2D()
 
-        Earth2dL_div = plot(Earth2dL.plot_body_orbit(
-            Earth, date_launch), output_type='div')
+            Earth2dL_div = plot(Earth2dL.plot_body_orbit(
+                Earth, date_launch), output_type='div')
 
-        # Mars Launch
-        Mars2dL = OrbitPlotter2D()
+            # Mars Launch
+            Mars2dL = OrbitPlotter2D()
 
-        Mars2dL_div = plot(Mars2dL.plot_body_orbit(
-            Mars, date_launch), output_type='div')
+            Mars2dL_div = plot(Mars2dL.plot_body_orbit(
+                Mars, date_launch), output_type='div')
 
-        # Frame Launch
-        Frame2dL = OrbitPlotter2D()
+            # Frame Launch
+            Frame2dL = OrbitPlotter2D()
 
-        Frame2dL.plot_body_orbit(Mars, date_launch)
+            Frame2dL.plot_body_orbit(Mars, date_launch)
 
-        Frame2dL_div = plot(Frame2dL.plot_body_orbit(
-            Earth, date_launch), output_type='div')
+            Frame2dL_div = plot(Frame2dL.plot_body_orbit(
+                Earth, date_launch), output_type='div')
 
-        # Earth Arrival
-        Earth2dA = OrbitPlotter2D()
+            # Earth Arrival
+            Earth2dA = OrbitPlotter2D()
 
-        Earth2dA_div = plot(Earth2dA.plot_body_orbit(
-            Earth, date_arrival), output_type='div')
+            Earth2dA_div = plot(Earth2dA.plot_body_orbit(
+                Earth, date_arrival), output_type='div')
 
-        # Mars Arrival
-        Mars2dA = OrbitPlotter2D()
+            # Mars Arrival
+            Mars2dA = OrbitPlotter2D()
 
-        Mars2dA_div = plot(Mars2dA.plot_body_orbit(
-            Mars, date_launch), output_type='div')
+            Mars2dA_div = plot(Mars2dA.plot_body_orbit(
+                Mars, date_launch), output_type='div')
 
-        # Frame Arrival
-        Frame2dA = OrbitPlotter2D()
+            # Frame Arrival
+            Frame2dA = OrbitPlotter2D()
 
-        Frame2dA.plot_body_orbit(Mars, date_launch)
+            Frame2dA.plot_body_orbit(Mars, date_launch)
 
-        Frame2dA_div = plot(Frame2dA.plot_body_orbit(
-            Earth, date_launch), output_type='div')
+            Frame2dA_div = plot(Frame2dA.plot_body_orbit(
+                Earth, date_launch), output_type='div')
 
-        # THE GOOD STUFF - Visualizing Desired Trajectory
-        earth = Ephem.from_body(Earth, time_range(
-            date_launch, end=date_arrival))
-        mars = Ephem.from_body(Mars, time_range(date_launch, end=date_arrival))
+            # THE GOOD STUFF - Visualizing Desired Trajectory
+            earth = Ephem.from_body(Earth, time_range(
+                date_launch, end=date_arrival))
+            mars = Ephem.from_body(Mars, time_range(
+                date_launch, end=date_arrival))
 
-        # Solve for departure and target orbits
-        ss_earth = Orbit.from_ephem(Sun, earth, date_launch)
-        ss_mars = Orbit.from_ephem(Sun, mars, date_arrival)
+            # Solve for departure and target orbits
+            ss_earth = Orbit.from_ephem(Sun, earth, date_launch)
+            ss_mars = Orbit.from_ephem(Sun, mars, date_arrival)
 
-        # Solve for the transfer maneuver
-        man_lambert = Maneuver.lambert(ss_earth, ss_mars)
+            # Solve for the transfer maneuver
+            man_lambert = Maneuver.lambert(ss_earth, ss_mars)
 
-        # Get the transfer and final orbits
-        ss_trans, ss_target = ss_earth.apply_maneuver(
-            man_lambert, intermediate=True)
+            # Get the transfer and final orbits
+            ss_trans, ss_target = ss_earth.apply_maneuver(
+                man_lambert, intermediate=True)
 
-        plotter = OrbitPlotter3D()
-        plotter.set_attractor(Sun)
+            plotter = OrbitPlotter3D()
+            plotter.set_attractor(Sun)
 
-        plotter.plot_ephem(earth, date_launch,
-                           label="Earth at launch position")
-        plotter.plot_ephem(mars, date_arrival,
-                           label="Mars at arrival position")
-        plotter.plot_trajectory(
-            ss_trans.sample(max_anomaly=180 * u.deg), color="black", label="Transfer orbit"
-        )
-        plotter_div = plot(plotter.set_view(
-            30 * u.deg, 260 * u.deg, distance=3 * u.km), output_type='div')
+            plotter.plot_ephem(earth, date_launch,
+                               label="Earth at launch position")
+            plotter.plot_ephem(mars, date_arrival,
+                               label="Mars at arrival position")
+            plotter.plot_trajectory(
+                ss_trans.sample(max_anomaly=180 * u.deg), color="black", label="Transfer orbit"
+            )
+            plotter_div = plot(plotter.set_view(
+                30 * u.deg, 260 * u.deg, distance=3 * u.km), output_type='div')
 
-        return render(request, 'BluePages/simulator.html', {'launch_date': launch_date, 'arrival_date': arrival_date, 'Earth2dL_div': Earth2dL_div, 'Mars2dL_div': Mars2dL_div, 'Frame2dL_div': Frame2dL_div, 'Earth2dA_div': Earth2dA_div, 'Mars2dA_div': Mars2dA_div, 'Frame2dA_div': Frame2dA_div, 'plotter_div': plotter_div})
+            return render(request, 'BluePages/simulator.html', {'launch_date': launch_date, 'arrival_date': arrival_date, 'Earth2dL_div': Earth2dL_div, 'Mars2dL_div': Mars2dL_div, 'Frame2dL_div': Frame2dL_div, 'Earth2dA_div': Earth2dA_div, 'Mars2dA_div': Mars2dA_div, 'Frame2dA_div': Frame2dA_div, 'plotter_div': plotter_div})
+        else:
+            return render(request, 'BluePages/simulator.html', {})
 
     else:
         # SolarSys Code
